10b74169d17b23734d48b8e5a324c59509654f35
priyajha219 <priyaj22@navgurukul.org>
2023-06-20 13:50:54 +0530

    initial commit

---
+++ shuffle.py
@@ -0,0 +1,17 @@
+# import random
+# room_no=[1,2,3]
+# final=[]
+# students=["priya","madhu","aman","mohit","shikha","lovely"]
+
+# for i in range(len(room_no)):
+#     data=random.choice(room_no)
+#     stud=random.sample(students,2)
+#     dic={
+#         "room_no":data,
+#         "student_name":stud
+#     }
+#     final.append(dic)
+#     rem=room_no.remove(data)
+#     dele=students.remove(stud[0])
+#     dele=students.remove(stud[1])
+# print(final)
